2c4bba6d2e8dd0de914e12f33b7560d177ac2517
Richard Chalk <barmybrit@gmail.com>
2024-08-28 12:52:27 +0200

    Add project files.

---
+++ BlazorIntro/Models/EmployeeNameAndTitle.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BlazorIntro.Models;
+
+public partial class EmployeeNameAndTitle
+{
+    public string FirstName { get; set; } = null!;
+
+    public string LastName { get; set; } = null!;
+
+    public string? Title { get; set; }
+
+    public string? Address { get; set; }
+}
